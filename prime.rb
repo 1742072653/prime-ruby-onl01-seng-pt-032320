0e8652addc60f910d7d607bdf3d7e275c1479db9
Wenhao Guo <gwh1742072653@gmail.com>
2020-04-01 21:29:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  i = 2
+  if(num <= 1)
+    return false 
+  elsif (num == 2)
+    return true
+  else
+    while(i < num)
+      if(num % i == 0)
+        return false 
+      end
+    end
+    return true
+  end
+end
